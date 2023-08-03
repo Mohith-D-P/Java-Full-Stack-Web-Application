e58c911a9c6ba1356405bd1c8d107d4d47afc6f8
MOHITH D P <119958455+Mohith-D-P@users.noreply.github.com>
2023-08-03 18:21:34 +0530

    Application.java

---
+++ LearnersSphereApplication.java
@@ -0,0 +1,13 @@
+package com.learnersSphere.LearnersSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnersSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnersSphereApplication.class, args);
+	}
+
+}
